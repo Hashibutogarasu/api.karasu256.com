63ff2de7f9722669ca41644f3a16acf37b724720
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-05 01:57:23 +0900

    APIのバージョンをpackage.jsonから取得するように変更

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 import { patchNestJsSwagger } from "nestjs-zod";
 import * as bodyParser from 'body-parser';
+import * as packageJson from '../package.json';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   patchNestJsSwagger();
@@ -29,7 +31,7 @@ async function bootstrap() {
     .setTitle(`Karasu Lab API ${process.env.NODE_ENV}`)
     .setLicense("MIT", "https://opensource.org/licenses/MIT")
     .setDescription("API documentation for Karasu Lab")
-    .setVersion("1.0")
+    .setVersion(packageJson.version)
     .addServer(process.env.BASE_URL)
     .addBearerAuth({
       type: 'http',
