d43d2107adc8da9fc07b37ba896b231c1fa674aa
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-05 17:52:43 +0900

    ManyToManyリレーションのプロパティをPromise型から配列型に変更し、nullableを維持

--- src/entities/wiki/genshin/versions.entity.ts
+++ src/entities/wiki/genshin/versions.entity.ts
@@ -45,7 +45,7 @@ export class VersionsEntity extends BaseEntity implements IBase {
       referencedColumnName: 'id',
     },
   })
-  weapons?: Promise<Weapon[]> | null;
+  weapons?: Weapon[] | null;
 
   @ManyToMany(() => ArtifactSets, { eager: true, nullable: true })
   @JoinTable({
@@ -59,7 +59,7 @@ export class VersionsEntity extends BaseEntity implements IBase {
       referencedColumnName: 'id',
     },
   })
-  artifact_sets?: Promise<ArtifactSets[]> | null;
+  artifact_sets?: ArtifactSets[] | null;
 
   @ManyToMany(() => Country, { eager: true, nullable: true })
   @JoinTable({
@@ -73,7 +73,7 @@ export class VersionsEntity extends BaseEntity implements IBase {
       referencedColumnName: 'id',
     },
   })
-  countries?: Promise<Country[]> | null;
+  countries?: Country[] | null;
 
   @ManyToMany(() => Artifacts, { eager: true, nullable: true })
   @JoinTable({
@@ -87,5 +87,5 @@ export class VersionsEntity extends BaseEntity implements IBase {
       referencedColumnName: 'id',
     },
   })
-  artifacts?: Promise<Artifacts[]> | null;
+  artifacts?: Artifacts[] | null;
 }
