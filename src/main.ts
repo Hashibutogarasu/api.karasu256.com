745405d8833330b3934837bd8a609dd423001a2c
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-08 07:39:35 +0900

    サブプロジェクトのコミットを更新し、バージョンを3.3.5に変更しました。

--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
     .setTitle(`Karasu Lab API ${node_env}`)
     .setLicense("MIT", "https://opensource.org/licenses/MIT")
     .setDescription("API documentation for Karasu Lab")
-    .setVersion("3.3.1")
+    .setVersion("3.3.5")
     .addServer(process.env.BASE_URL)
     .addBearerAuth({
       type: 'http',
