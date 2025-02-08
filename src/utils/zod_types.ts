df978513a1df7c5d7c754dd24f559bc9230f56e5
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-08 09:01:40 +0900

    不要なコードを削除し、レアリティタイプの定義を整理しました

--- src/utils/zod_types.ts
+++ src/utils/zod_types.ts
@@ -1,16 +1,5 @@
-import { z, ZodTypeAny } from "zod";
+import { z } from "zod";
 
-export const numericString = (schema: ZodTypeAny) => z.preprocess((a) => {
-  if (typeof a === 'string') {
-    return parseInt(a, 10)
-  } else if (typeof a === 'number') {
-    return a;
-  } else {
-    return undefined;
-  }
-}, schema);
+const rarityType = z.number().int().min(1).max(5);
 
-const number_type = numericString(z.number());
-const rarityType = numericString(z.number({ message: "レアリティは数値である必要があります" }).optional());
-
-export { rarityType, number_type }
+export { rarityType }
