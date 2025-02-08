dd71f939c4b68a42165fbdc24d38b28cc11f9111
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-08 13:19:31 +0900

    サブプロジェクトのコミットを更新

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ async function bootstrap() {
     .setTitle("Karasu Lab API")
     .setLicense("MIT", "https://opensource.org/licenses/MIT")
     .setDescription("API documentation for Karasu Lab")
-    .setVersion("3.3.14")
+    .setVersion("3.3.15")
     .addServer(process.env.BASE_URL)
     .addBearerAuth({
       type: 'http',
