3bfeabd230e0888649a11e625631ee57da84d5c7
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-12 22:34:23 +0900

    HI3キャラクターコントローラーにAPIボディスキーマを追加

--- src/wiki/public/honkai_impact_3rd/hi3_characters/hi3_characters.controller.ts
+++ src/wiki/public/honkai_impact_3rd/hi3_characters/hi3_characters.controller.ts
@@ -3,7 +3,9 @@ import { IBasePublicCaS } from '@/types/ibase_public_cas';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Hi3CharactersService } from './hi3_characters.service';
 import { GetParamsDto } from '@/utils/dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { zodToOpenAPI } from 'nestjs-zod';
+import { getSchema } from './hi3_characters.dto';
 
 @Controller('wiki/public/honkai_impact_3rd/hi3_characters')
 @ApiTags("hi3_characters")
@@ -17,11 +19,17 @@ export class Hi3CharactersController implements IBasePublicCaS<HI3Characters> {
     return await this.hi3CharactersService.getAll();
   }
 
+  @ApiBody({
+    schema: zodToOpenAPI(getSchema)
+  })
   @Post('get')
   async get(@Body() query: GetParamsDto<HI3Characters, ["skills", "stigmatas", "weapons", "createdAt", "updatedAt"]>): Promise<HI3Characters[]> {
     return await this.hi3CharactersService.get(query);
   }
 
+  @ApiBody({
+    schema: zodToOpenAPI(getSchema)
+  })
   @Post('getOne')
   async getOne(@Body() query: GetParamsDto<HI3Characters, ["skills", "stigmatas", "weapons", "createdAt", "updatedAt"]>): Promise<HI3Characters> {
     return await this.hi3CharactersService.getOne(query);
