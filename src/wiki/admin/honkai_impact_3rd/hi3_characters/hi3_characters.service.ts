b96e86ac74ecd1889274b43bc18a59461fd08770
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-16 00:23:06 +0900

    HI3キャラクターサービスにスタigmataリポジトリを追加し、スタigmataの作成処理を修正

--- src/wiki/admin/honkai_impact_3rd/hi3_characters/hi3_characters.service.ts
+++ src/wiki/admin/honkai_impact_3rd/hi3_characters/hi3_characters.service.ts
@@ -1,5 +1,6 @@
 import { HI3Characters } from '@/entities/wiki/hi3/hi3_characters.entity';
 import { HI3SkillsEntity } from '@/entities/wiki/hi3/hi3_skills.entity';
+import { HI3StigmatasEntity } from '@/entities/wiki/hi3/hi3_stigmatas.entity';
 import { HI3WeaponsEntity } from '@/entities/wiki/hi3/hi3_weapons.entity';
 import { IBaseAdminCaS } from '@/types/ibase_admin_cas';
 import { CreateDto, UpdateDto, DeleteDto } from '@/utils/dto';
@@ -18,6 +19,9 @@ export class Hi3CharactersService implements IBaseAdminCaS<HI3Characters> {
 
     @InjectRepository(HI3WeaponsEntity)
     private readonly hi3WeaponsRepository: Repository<HI3WeaponsEntity>,
+
+    @InjectRepository(HI3StigmatasEntity)
+    private readonly hi3StigmatasRepository: Repository<HI3StigmatasEntity>,
   ) { }
 
   async create(dto: CreateDto<HI3Characters>): Promise<HI3Characters> {
@@ -30,15 +34,15 @@ export class Hi3CharactersService implements IBaseAdminCaS<HI3Characters> {
     const newSkils = skills ? await Promise.all(Array.from(skills).map(async skill => {
       const { characters, ...ref } = skill;
 
-      return await this.hi3SkillsRepository.save({
-        ...ref
+      return this.hi3SkillsRepository.create({
+        ...ref,
       });
     })) : [];
 
     const newWeapons = weapons ? await Promise.all(Array.from(weapons).map(async weapon => {
       const { characters, ...ref } = weapon;
 
-      return await this.hi3WeaponsRepository.save({
+      return this.hi3WeaponsRepository.create({
         ...ref
       });
     })) : [];
@@ -46,7 +50,7 @@ export class Hi3CharactersService implements IBaseAdminCaS<HI3Characters> {
     const newStigmatas = stigmatas ? await Promise.all(Array.from(stigmatas).map(async stigmata => {
       const { characters, ...ref } = stigmata;
 
-      return await this.hi3SkillsRepository.save({
+      return this.hi3StigmatasRepository.create({
         ...ref
       });
     })) : [];
