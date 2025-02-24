86f373df83bfe68dca2080caab1b5bd04f69035d
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-25 01:23:40 +0900

    regions.dto.tsからicon_urlフィールドを削除

--- src/wiki/admin/genshin/regions/regions.dto.ts
+++ src/wiki/admin/genshin/regions/regions.dto.ts
@@ -4,7 +4,6 @@ const regionSchema = z.object({
   id: z.string(),
   name: z.string(),
   description: z.string().nullish(),
-  icon_url: z.string().url(),
   createdAt: z.string(),
   updatedAt: z.string(),
 });
