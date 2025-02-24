675875c8bac2101780fa81ffcf02ef121c759a11
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-25 01:35:54 +0900

    サブプロジェクトのコミットを更新し、APIバージョンを4.2.0に変更

--- src/main.ts
+++ src/main.ts
@@ -74,7 +74,7 @@ async function bootstrap() {
     .setTitle("Karasu Lab API")
     .setLicense("MIT", "https://opensource.org/licenses/MIT")
     .setDescription("API documentation for Karasu Lab")
-    .setVersion("4.1.1")
+    .setVersion("4.2.0")
     .addServer(process.env.BASE_URL)
     .addBearerAuth({
       type: 'http',
